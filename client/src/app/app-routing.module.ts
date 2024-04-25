193c1c6652a605fdaec35110eefca99e1521a3c7
anandmindfire <anand.k@mindfiresolutions.com>
2024-04-25 15:45:46 +0530

    feat:add bootstrap,add clint side routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,8 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { TicketListComponent } from './components/ticket-list/ticket-list.component';
+import { TicketCreateComponent } from './components/ticket-create/ticket-create.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'home', component: HomeComponent  },
+  { path: 'register', component: RegisterComponent },
+  { path: 'tickets', component: TicketListComponent },
+  { path: 'tickets/create', component: TicketCreateComponent },
+  { path: '**', redirectTo: '' } // Redirect to Login component if route not found
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
